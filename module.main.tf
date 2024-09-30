40897008adba1d4b42d35cb7872774a43a4e9363
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2024-09-30 17:13:49 +0400

    Initial commit

--- module.main.tf
+++ module.main.tf
@@ -2,14 +2,14 @@
 /*Networking Module | Stream Pool*/
 module "stream-pools" {
   source            = "./modules/stream-pool"
-  core-stream-pools = local.lst-stream-pools
   core-compartments = var.data-compartments
+  core-stream-pools = local.lst-stream-pools
 }
 
 /*Networking Module | Stream*/
 module "streams" {
   source            = "./modules/stream"
+  core-compartments = var.data-compartments
   core-streams      = local.lst-streams
   core-stream-pools = module.stream-pools.ids
-  core-compartments = var.data-compartments
 }
